6b6d22351ba1cb87c13a27f4b3b33599367c0d5b
Andy Doswell <andydoswell@hotmail.com>
2025-01-22 10:45:28 +0000

    Update TeensyAMProcessor_with_synth.ino

--- TeensyAMProcessor_with_synth.ino
+++ TeensyAMProcessor_with_synth.ino
@@ -1,6 +1,5 @@
 /*
-Simple (and somwhat agressive) audio filter an processor for the MW transmitter project
-Also generates carrier with AD9833
+A multiband compressor attempting to emulate AM transmission. 
  */
 
 #include <Audio.h>
@@ -11,16 +10,17 @@ Also generates carrier with AD9833
 #include <MD_AD9833.h>
 
 AudioInputI2S i2s1;                //xy=441,196
-AudioAnalyzePeak peak3;            //xy=579,289
+AudioAnalyzePeak peak3;            //xy=591,273
 AudioAmplifier amp2;               //xy=629,195
 AudioAnalyzePeak peak2;            //xy=784,276
 AudioFilterBiquad biquad1;         //xy=798,192
-AudioAmplifier amp1;               //xy=946,197
-AudioFilterStateVariable filter1;  //xy=1179,193
-AudioAnalyzePeak peak1;            //xy=1480,336
-AudioAnalyzePeak peak4;            //xy=1495,372
-AudioAnalyzePeak peak5;            //xy=1511,416
-AudioMixer4 mixer1;                //xy=1576,216
+AudioAmplifier amp1;               //xy=993,191
+AudioAnalyzePeak peak6;            //xy=1110,258
+AudioFilterStateVariable filter1;  //xy=1185,193
+AudioAnalyzePeak peak5;            //xy=1481,368
+AudioAnalyzePeak peak4;            //xy=1483,330
+AudioAnalyzePeak peak1;            //xy=1484,292
+AudioMixer4 mixer1;                //xy=1579,214
 AudioOutputI2S i2s2;               //xy=1809,214
 AudioConnection patchCord1(i2s1, 1, amp2, 0);
 AudioConnection patchCord2(i2s1, 1, peak3, 0);
@@ -28,38 +28,41 @@ AudioConnection patchCord3(amp2, peak2);
 AudioConnection patchCord4(amp2, biquad1);
 AudioConnection patchCord5(biquad1, amp1);
 AudioConnection patchCord6(amp1, 0, filter1, 0);
-AudioConnection patchCord7(filter1, 0, mixer1, 0);
-AudioConnection patchCord8(filter1, 0, peak1, 0);
-AudioConnection patchCord9(filter1, 1, mixer1, 1);
-AudioConnection patchCord10(filter1, 1, peak4, 0);
-AudioConnection patchCord11(filter1, 2, mixer1, 2);
-AudioConnection patchCord12(filter1, 2, peak5, 0);
-AudioConnection patchCordgainLimit(mixer1, 0, i2s2, 1);
+AudioConnection patchCord7(amp1, peak6);
+AudioConnection patchCord8(filter1, 0, mixer1, 0);
+AudioConnection patchCord9(filter1, 0, peak1, 0);
+AudioConnection patchCord10(filter1, 1, mixer1, 1);
+AudioConnection patchCord11(filter1, 1, peak4, 0);
+AudioConnection patchCord12(filter1, 2, mixer1, 2);
+AudioConnection patchCord13(filter1, 2, peak5, 0);
+AudioConnection patchCord14(mixer1, 0, i2s2, 1);
 AudioControlSGTL5000 audioShield;
 
 const uint8_t PIN_DATA = 26;   ///< SPI Data pin number
 const uint8_t PIN_CLK = 27;    ///< SPI Clock pin number
 const uint8_t PIN_FSYNC = 28;  ///< SPI Load pin number (FSYNC in AD9833 usage)
-MD_AD9833 AD(PIN_DATA, PIN_CLK, PIN_FSYNC);
-
+MD_AD9833	AD(PIN_DATA, PIN_CLK, PIN_FSYNC);
 const int myInput = AUDIO_INPUT_LINEIN;  //const int myInput = AUDIO_INPUT_MIC;
-float peakMax = 1;
+float peakMax = .9;
 float peakGain;
-float rate = 0.01;
-int lowpass = 6000;
+float rate = 1;
+int lowpass = 5000;
+int highpass = 100;
 float filterGain;
-int LEDTime;
+unsigned int LEDTime;
 float midGain;
 float hiGain;
-float gainLimit = 5.5;
+int gainLimit = 3;
+float intGain;
 int fCal = 8;
 
 void setup() {
+  Serial.begin (115200);
   AudioMemory(128);
   audioShield.enable();
   audioShield.inputSelect(myInput);
   audioShield.volume(0.5);
-  biquad1.setHighpass(0, 80, 0.7);
+  biquad1.setHighpass(0, highpass, 0.7);
   biquad1.setLowpass(1, lowpass, 0.7);
   biquad1.setLowpass(2, lowpass, 0.7);
   biquad1.setLowpass(3, lowpass, 0.7);
@@ -68,6 +71,8 @@ void setup() {
   mixer1.gain(0, 0.5);
   mixer1.gain(1, 0.5);
   mixer1.gain(2, 0.5);
+  filter1.frequency(lowpass / 2);
+  filter1.resonance(1);
   AD.begin();
     for (int i = 34; i <= 41; i++) {  // set inputs for DIP switches
     pinMode(i, INPUT_PULLUP);
@@ -76,14 +81,14 @@ void setup() {
 }
 
 void loop() {
-  //Serial.print(AudioProcessorUsageMax()); Serial.print (" "); Serial.println (AudioMemoryUsageMax());
+
   if (peak1.available()) {
     float monoPeak = peak1.read();
     if (monoPeak < peakMax) {
       peakGain += rate;
     } else {
       peakGain -= (5 * rate);
-      if (peakGain <= 0) { peakGain = 0; }
+      if (peakGain <= 0.1) { peakGain = 0.1; }
     }
     if (peakGain > gainLimit) { peakGain = gainLimit; }
     mixer1.gain(0, peakGain);
@@ -92,22 +97,21 @@ void loop() {
   if (peak2.available()) {
     float filterPeak = (peak2.read());
 
-    if (filterPeak <= .5) {
-      filterGain += 1;
+    if (filterPeak <= .7) {
+      filterGain += .5;
     } else {
-      filterGain -= 1;
+      filterGain -= 2;
     }
-    if (filterGain > 4) { filterGain = 4; }
+    if (filterGain > 2) { filterGain = 2; }
     amp2.gain((filterGain / 10));
   }
   if (peak3.available()) {
     float inputClip = peak3.read();
-
-    if (inputClip >= .7) {
+    if (inputClip >= .5) {
       digitalWrite(gainLimit, HIGH);
       LEDTime = millis();
     } else {
-      if (LEDTime + 50 <= millis()) {
+      if (LEDTime + 50 == millis()) {
         digitalWrite(gainLimit, LOW);
       }
     }
@@ -118,7 +122,7 @@ void loop() {
       midGain += rate;
     } else {
       midGain -= (5 * rate);
-      if (midGain <= 0) { midGain = 0; }
+      if (midGain <= 0.1) { midGain = 0.1; }
     }
     if (midGain > gainLimit) { midGain = gainLimit; }
     mixer1.gain(1, midGain);
@@ -128,11 +132,22 @@ void loop() {
     if (monoPeak < peakMax) {
       hiGain += rate;
     } else {
-      hiGain -= (5 * rate);
-      if (hiGain <= 0) { midGain = 0; }
+      hiGain -= (rate);
+      if (hiGain <= .1) { hiGain = 0.1; }
     }
     if (hiGain > gainLimit) { hiGain = gainLimit; }
     mixer1.gain(2, hiGain);
+  }
+  if (peak6.available()) {
+    float monoPeak = peak6.read();
+    if (monoPeak < 0.5) {
+      intGain += rate;
+    } else {
+      intGain -= (5 * rate);
+      if (intGain <= 0.1) { intGain = 0.1; }
+    }
+    if (intGain > gainLimit) { intGain = gainLimit; }
+    amp1.gain(intGain);
   }
 }
 void getFreq() {
@@ -154,5 +169,6 @@ void getFreq() {
     frequency = (((channel - 128) * 10) + 520);
     frequency *= 1000;
   }
+  Serial.println(frequency);
   AD.setFrequency(MD_AD9833::CHAN_0, (frequency+fCal));  // set ad9833
 }
